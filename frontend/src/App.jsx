ac010616457176f9cb2a45fcd820e440e6421e42
ottock <ottock@outlook.com>
2026-02-25 19:39:31 -0300

    fix(app.jsx): fix router import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "primeicons/primeicons.css";
 import { PrimeReactProvider } from "primereact/api";
 
 // project imports
-import AppRouter from "./presentation/routes/AppRouter.jsx"
+import AppRouter from "./presentation/routes/appRouter.jsx"
 
 export default function App() {
   return (
