5a6ae0fe4b7fb0433056bce2f0b77c53797252cb
ottock <ottock@outlook.com>
2026-03-03 20:28:59 -0300

    fix: remove trailing whitespace in launchProject.bat

--- scripts/windows/launchProject.bat
+++ scripts/windows/launchProject.bat
@@ -3,5 +3,5 @@ title launchProject
 echo +++ EXECUTING launchProject.bat +++
 cd /d C:\Users\ottoc\projects\ToDoJS\scripts\windows
 start cmd /k "launchBackend.bat"
-start cmd /k "launchFrontend.bat"   
+start cmd /k "launchFrontend.bat"
 echo +++ EXECUTED launchProject.bat +++
